7f40abdd7c08c761109bde7f1313729e7fa35e34
Alexandre Souza <alexandrehg2001@gmail.com>
2020-11-05 22:35:16 -0300

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="left">
+        <h1>bitparty</h1>
+        <div className="header">
+          <p>festival de <b>rock 2021</b></p>
+        </div>
+        <div className="description">
+          <p>
+            Você não vai perder essa né? Fique por dentro de todas as novidades, sobre artistas
+            e cantores que estarão presentes no BitParty Rock 2021 !
+          <br />
+          E receba pequenos teasers dos eventos mais IRADOS! que vão ocorrer no local,
+          vem com a gente, é BitParty, é rolê garantido! ;)
+          </p>
+        </div>
+        <div className="input_send">
+          <input type="email" required placeholder="Example@gmail.com" />
+          <button>
+            <span class="material-icons">
+              arrow_forward_ios
+            </span>
+          </button>
+        </div>
+        <p className="footer">
+          <b>
+            Todos os direitos reservados a © BItParty - Saiba mais <a href="">aqui</a>
+          </b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="right">
+      </div>
+
     </div>
   );
 }
